ba18a6c5a911b5dbee5466d495298aba4ef90681
Pedro <pedroantonionereto46@gmail.com>
2024-09-06 09:52:09 -0300

    primeiro commit

---
+++ src/main/java/br/sesi/ped/java_aulas_poo/Principal.java
@@ -0,0 +1,7 @@
+package br.sesi.ped.java_aulas_poo;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/br/sesi/ped/modelo/Pessoa.java
@@ -0,0 +1,8 @@
+package br.sesi.ped.modelo;
+
+public class Pessoa {
+    float peso;
+    float altura;
+    int idade;
+    String nome;
+}
